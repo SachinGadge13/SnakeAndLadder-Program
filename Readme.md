5294eebd06746f09746fe35206b3aeaa5cfd1e49
SachinGadge13 <gadges14@gmail.com>
2022-09-07 16:52:40 +0530

    Added: Start Position

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Snake And ladder program execution
---
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLader.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLader {
+
+	public static void main(String[] args) {
+		int position1 = 0;
+		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("Position of Player1 is: " + position1);
+	}
+
+}
