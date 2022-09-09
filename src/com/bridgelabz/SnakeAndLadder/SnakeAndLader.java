7cf77863c604e6151a9bd044b3b0f17fde4ec253
SachinGadge13 <gadges14@gmail.com>
2022-09-09 17:44:39 +0530

    Added:Random Die Roll Program

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLader.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLader.java
@@ -1,11 +1,20 @@
 package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLader {
+	public static int rollCheck() {
+		int rollValue = (int) (Math.floor(Math.random() * 10 % 7));
+		return rollValue;
+
+	}
 
 	public static void main(String[] args) {
 		int position1 = 0;
 		System.out.println("Welcome to Snake and Ladder Game");
-		System.out.println("Position of Player1 is: " + position1);
+		System.out.println("Position of Player1 is:" + position1);
+		int rollDie = rollCheck();
+		if (rollDie > 0)
+			System.out.println("Value at die is: " + rollDie);
+		else
+			System.out.println("Roll the die again!");
 	}
-
 }
