93f144e14e4b351c11c08cfbdf3da808d5664659
SachinGadge13 <gadges14@gmail.com>
2022-09-10 00:08:36 +0530

    Added:Final Report which Player Won

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLader.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLader.java
@@ -2,6 +2,70 @@ package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLader {
 
+	static int player1 = 0, player2 = 0, winningPosition = 10, flag = 0, counter = 0;
+
+	public static void playgame() {
+		if (flag == 0) {
+			counter++;
+			int rollDie = rollCheck();
+			System.out.println("Value at die is: " + rollDie);
+			int checkOption = options();
+			switch (checkOption) {
+			case 0:
+				System.out.println("No play");
+				flag = 1;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				player1 = player1 + rollDie;
+				if (player1 > winningPosition)
+					player1 = player1 - rollDie;
+				System.out.println("Player1 is at position: " + player1);
+				break;
+			case 2:
+				System.out.println("Snake");
+				player1 = player1 - rollDie;
+				if (player1 < 0)
+					player1 = 0;
+				System.out.println("Player1 is at position: " + player1);
+				flag = 1;
+				break;
+			}
+			if (player1 == winningPosition) {
+				System.out.println("Player1 is winner");
+			}
+		} else {
+			counter++;
+			int rollDie = rollCheck();
+			System.out.println("Value at die is: " + rollDie);
+			int checkOption = options();
+			switch (checkOption) {
+			case 0:
+				System.out.println("No play");
+				flag = 0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				player2 = player2 + rollDie;
+				if (player2 > winningPosition)
+					player2 = player2 - rollDie;
+				System.out.println("Player2 is at position: " + player2);
+				break;
+			case 2:
+				System.out.println("Snake");
+				player2 = player2 - rollDie;
+				if (player2 < 0)
+					player2 = 0;
+				System.out.println("Player2 is at position: " + player2);
+				flag = 0;
+				break;
+			}
+			if (player2 == winningPosition) {
+				System.out.println("Player2 is winner");
+			}
+		}
+	}
+
 	public static int rollCheck() {
 		int rollValue = (int) (Math.floor(Math.random() * 10 % 6 + 1));
 		return rollValue;
@@ -14,34 +78,13 @@ public class SnakeAndLader {
 	}
 
 	public static void main(String[] args) {
-		int position1 = 0, counter = 0;
 		System.out.println("Welcome to Snake and Ladder Game");
-		System.out.println("Position of Player1 is:" + position1);
-		int winningPosition = 100;
-		while (position1 != winningPosition) {
-			int rollDie = rollCheck();
-			System.out.println("Value at die is: " + rollDie);
-			int checkOption = options();
-			switch (checkOption) {
-			case 0:
-				System.out.println("No play");
-				break;
-			case 1:
-				System.out.println("Ladder");
-				position1 = position1 + rollDie;
-				if (position1 > winningPosition)
-					position1 = position1 - rollDie;
-				break;
-			case 2:
-				System.out.println("Snake");
-				position1 = position1 - rollDie;
-				if (position1 < 0)
-					position1 = 0;
+		while (true) {
+			if (player1 == winningPosition || player2 == winningPosition) {
 				break;
 			}
-			System.out.println("Current Position of player1 is: " + position1);
-			counter++;
+			playgame();
 		}
-		System.out.println("Numbere of times the dice was played are: " + counter);
+		System.out.println("Number of time dice has rolled is: " + counter);
 	}
 }
